b06bc91b715c5d080fcda7ed53722db8ce529152
josephe44 <josephe442@gmail.com>
2023-05-19 12:51:48 +0100

    header and footer

---
+++ components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Link from "next/link";
+import styles from "../styles/Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Link href="/">
+          <span>DJ Events</span>
+        </Link>
+      </div>
+
+      <nav>
+        <ul>
+          <li>
+            <Link href="/events">
+              <span>Events</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Head from "next/head";
+import Header from "./Header";
+import Footer from "./Footer";
 import styles from "../styles/Layout.module.css";
 
 export default function Layout({ title, keywords, description, children }) {
@@ -13,7 +15,9 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
+      <Header />
       <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 }
