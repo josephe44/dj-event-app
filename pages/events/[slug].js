e2bf66e2fc05af8a64e83fb5ac6ae2b4efe639a1
josephe44 <josephe442@gmail.com>
2023-05-19 15:38:19 +0100

    added getStaticPaths

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,15 +1,64 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function SingleEventPage() {
+export default function SingleEventPage({ events }) {
   const router = useRouter();
-  console.log(router);
+
   return (
     <Layout title="Single event page">
       <h1>Single Event</h1>
-      {router.query.slug}
-      <button onClick={() => router.push("/")}>Click Me</button>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map((evt) => (
+        <div key={evt.id}>
+          <h1>{evt.name}</h1>
+          <h3>{evt.performers}</h3>
+          <p>{evt.description}</p>
+          <p>{evt.address}</p>
+          <p>{evt.date}</p>
+          <p>{evt.time}</p>
+        </div>
+      ))}
+
+      <button className="btn" onClick={() => router.push("/events")}>
+        Go Back
+      </button>
     </Layout>
   );
+}
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   const res = await fetch(`${API_URL}/api/events/${slug}`);
+//   const events = await res.json();
+//   return {
+//     props: {
+//       events,
+//     },
+//   };
+// }
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+  return {
+    props: {
+      events,
+    },
+    revalidate: 1,
+  };
 }
