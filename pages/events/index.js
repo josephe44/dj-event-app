d8f4d94217760887174f9fc2581b926575485d64
josephe44 <josephe442@gmail.com>
2023-05-19 14:58:53 +0100

    display event from api wuth getStaticProps

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,25 @@
-import React from "react";
+import Link from "next/link";
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
 
-export default function EventPage() {
+export default function EventPage({ events }) {
   return (
-    <Layout title="Event page">
-      <h1>Event</h1>
+    <Layout>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
