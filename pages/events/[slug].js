885f3fd79022358c7bab0df85de5087f9aecaf12
josephe44 <josephe442@gmail.com>
2023-05-19 12:20:07 +0100

    next link and router

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 export default function SingleEventPage() {
+  const router = useRouter();
+  console.log(router);
   return (
     <div>
       <h1>Single Event</h1>
+      {router.query.slug}
+      <button onClick={() => router.push("/")}>Click Me</button>
     </div>
   );
 }
