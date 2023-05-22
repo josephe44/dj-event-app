a39a0661830d0eacdc2eed4fee9b1e7f5300b2cc
josephe44 <josephe442@gmail.com>
2023-05-22 16:56:23 +0100

    added search component

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Search from "./Search";
 import styles from "@/styles/Header.module.css";
 
 export default function Header() {
@@ -10,6 +11,8 @@ export default function Header() {
           <span>DJ Events</span>
         </Link>
       </div>
+
+      <Search />
 
       <nav>
         <ul>
